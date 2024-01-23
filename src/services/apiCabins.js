e03c7841eaa2736d4a49176368eeb4d302874196
Programeris3000 <valdasbucius1@gmail.com>
2024-01-23 09:50:31 +0200

    Create cabin, upload image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,10 +1,43 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  console.log(newCabin);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  // https://zdmumewnoadrufptujzq.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+  //1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin.data, image: imagePath }]);
+  // console.log([{ ...newCabin.data, image: imagePath }]);
+  if (error) {
+    throw new Error("Cabin could not be created");
+  }
+
+  //2.Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //. Delete the cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
@@ -13,7 +46,6 @@ export async function getCabins() {
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
-    console.error(error);
     throw new Error("Cabin could not be deleted");
   }
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zdmumewnoadrufptujzq.supabase.co";
+export const supabaseUrl = "https://zdmumewnoadrufptujzq.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbXVtZXdub2FkcnVmcHR1anpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTU0NDcsImV4cCI6MjAyMTIzMTQ0N30.GM-2ggMsl51cGF7Rn2HSABokluVAXFZ4KmMiHqk7CQ8";
 const supabase = createClient(supabaseUrl, supabaseKey);
